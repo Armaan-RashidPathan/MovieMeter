97ab8a15e84136de99adca26d1a151fe3f897a46
tech1e94 <tech1e0351@gmail.com>
2025-02-06 09:59:55 +0530

    addded some effects to cards

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ function App() {
       <Header/>
       <MovieCards name="Movie1" genre={"Horror"} year={2000} img={sampleThumbnail}/>
       <MovieCards name="Movie2" genre={"Drama"} year={2001} img={sampleThumbnail}/>
+      <MovieCards name="Movie3" genre={"Action"} year={2002} img={sampleThumbnail}/>
+      <MovieCards name="Movie4" genre={"Fantasy"} year={2003} img={sampleThumbnail}/>
+      <MovieCards name="Movie5" genre={"Suspense"} year={2004} img={sampleThumbnail}/>
       <Footer/>
     </>
   )
